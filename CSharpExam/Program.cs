d7f6e1f2e7a9eae06c2cc2d27e8fa7ff5bc5025f
Tom Martin <Tom.Martin@streets-heaver.com>
2019-09-13 17:15:20 +0100

    a

--- CSharpExam/Program.cs
+++ CSharpExam/Program.cs
@@ -8,11 +8,11 @@ namespace CSharpExam
     {
         static void Main()
         {
-            var ImplementMultithreading = new Chapter1_1_ImplementMultithreading();
-            var ManageMultithreading = new Chapter1_2_ManageMultithreading();
-            var ImplementProgramFlow = new Chapter1_3_ImplementProgramFlow();
-            var CreateAndImplementEventsAndCallbacks = new Chapter1_4_CreateAndImplementEventsAndCallbacks();
-            var ImplementExceptionHandling = new Chapter1_5_ImplementExceptionHandling();
+            Chapter1_1_ImplementMultithreading ImplementMultithreading = new Chapter1_1_ImplementMultithreading();
+            Chapter1_2_ManageMultithreading ManageMultithreading = new Chapter1_2_ManageMultithreading();
+            Chapter1_3_ImplementProgramFlow ImplementProgramFlow = new Chapter1_3_ImplementProgramFlow();
+            Chapter1_4_CreateAndImplementEventsAndCallbacks CreateAndImplementEventsAndCallbacks = new Chapter1_4_CreateAndImplementEventsAndCallbacks();
+            Chapter1_5_ImplementExceptionHandling ImplementExceptionHandling = new Chapter1_5_ImplementExceptionHandling();
 
             //Chapter 1
 
